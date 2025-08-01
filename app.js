9c3285cf240f9f95de7ef4cf884069216cb39c84
gferreirasilva <gabrielf.kasai@gmail.com>
2025-08-01 08:19:14 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Import Express.js
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+// Route for GET requests
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
